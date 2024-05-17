89332f54c49b448bd2f7985cb3a07e1b334736bc
Surunel Bergins <92090382+surisurity@users.noreply.github.com>
2024-05-17 08:46:17 +0200

    app routes
    app backend

---
+++ app.js
@@ -0,0 +1,36 @@
+
+const mongoose =require("mongoose")
+
+mongoose.connect( "mongodb://localhost:27017/datashop/resturants/resturants")
+.then(()=>{
+    console.log("connection open")
+}).catch(err=>{
+    console.log(err)
+    console.log("error connection failed")
+})
+
+
+const resturantSchema =new mongoose.Schema({
+name:{
+    type:String,
+    required:true
+},
+location:{
+    type:String,
+    required:true
+},
+
+rating:{
+    type:String,
+    required:true
+},
+score:{
+    type:Number,
+    required:true
+}
+})
+
+const Resturants = mongoose.model('Resturants',resturantSchema);
+//const cattlebaron  = new Resturant({name:'Cattelbaron',location:'Kuilsriver',description:'Steakhouse',rating:4})
+
+ 
